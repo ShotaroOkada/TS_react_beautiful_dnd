08851c5a063194b605a5a88d658f5a09b5ea4b25
ShotaroOkada <b1016117@gmail.com>
2019-08-14 11:09:50 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+} 
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') as HTMLElement
+);
